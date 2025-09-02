eeea51ffee0ac91a4e156dea1f5ebcacdc18c7ef
Radu Baroncea <baroncea.radu@gmail.com>
2025-09-02 15:48:19 +0300

    fix redirect

---
+++ src/app/page.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+const Homepage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push(`/auth`);
+  }, []);
+
+  return null;
+};
+
+export default Homepage;
